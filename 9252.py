f185fd152e3f2307299ba7b7f91cf42129ff6f38
jk121925 <52795602+jk121925@users.noreply.github.com>
2024-02-23 22:28:01 +0900

    Update 9252.py

--- 9252.py
+++ 9252.py
@@ -9,25 +9,34 @@ s2 = input.readline().strip()
 
 
 dp = [[0 for _ in range(len(s1)+1)] for _ in range(len(s2)+1)]
+retans = 0
 for i in range(1,len(s2)+1):
     for j in range(1,len(s1)+1):
         if s2[i-1] == s1[j-1]:
-            dp[i][j] = dp[i-1][j] +1
+            dp[i][j] = dp[i-1][j-1] +1
+            retans = max(dp[i][j],retans)
         else:
             dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+            
 
-# maxcnt =1e9
-# anscnt =1e9
-# ansstr = ""
-# for i in range(len(s2)-1,-1,-1):
-#     for j in range(len(s1)-1,-1,-1):
-#         if s2[i] == s1[j] and maxcnt > dp[i][j]:
-#             maxcnt = dp[i][j]
-#             ansstr = s2[i] +ansstr
-#             if anscnt == 1e9:
-#                 anscnt = dp[i][j]
-#             break
-print(*dp,sep="\n")
+retstr = ""
 
-# print(anscnt)
-# print(ansstr)
+s1p=len(s1)
+s2p=len(s2)
+
+while s1p >0 and s2p >0:
+    if dp[s2p][s1p] ==dp[s2p][s1p-1]:
+      s1p-=1
+    # 이줄 때문에
+    elif dp[s2p][s1p] == dp[s2p-1][s1p]:
+      s2p-=1
+    else:
+        retstr =  s1[s1p-1] + retstr
+        s1p-=1
+        s2p-=1
+
+# print(*dp, sep="\n")
+print(retans)
+if retstr !="":
+    print(retstr)
+# print(*dp,sep="\n")
