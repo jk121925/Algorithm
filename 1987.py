41e8673803e03767e58568a449918a94cc65d3b3
jk121925 <52795602+jk121925@users.noreply.github.com>
2024-02-26 15:26:25 +0900

    Update 1987.py

--- 1987.py
+++ 1987.py
@@ -9,28 +9,27 @@ for _ in range(R):
     addarr = list(input.readline().strip())
     arr.append(addarr)
 
-visit = [0 for _ in range(26)]
+visit = [False for _ in range(26)]
 
 
 dx = [0,0,-1,1]
 dy = [1,-1,0,0]
-visit[ord(arr[0][0]) -65] = True
 ans =0
 def dfs(r,c,now):
     global R,C
     global ans
-    returnFalg = True
-    for i in range(4):
-        nr = r + dx[i]
-        nc = c + dy[i]
-        if nr >=0 and nr <R and nc >=0 and nc <C and not visit[ord(arr[nr][nc]) -65]: 
-            returnFalg = False 
-            visit[ord(arr[nr][nc])-65] = True
-            dfs(nr,nc,now+1)
-            visit[ord(arr[nr][nc])-65] = False
-    if returnFalg:
-        ans = max(now,ans)
+    if visit[ord(arr[r][c])-65] == True:
+        ans = max(ans,now)
         return
+    else:
+        visit[ord(arr[r][c])-65] = True
+        for i in range(4):
+            nr = r + dx[i]
+            nc = c + dy[i]
+            if nr >=0 and nr <R and nc >=0 and nc <C: 
+                dfs(nr,nc,now+1)
+        visit[ord(arr[r][c])-65] = False
+
 
 dfs(0,0,0)
-print(ans+1)
+print(ans)
