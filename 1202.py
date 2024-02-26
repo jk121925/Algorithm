b5ac986c04894f060bc4d7729bceff903e1fd896
jk121925 <52795602+jk121925@users.noreply.github.com>
2024-02-26 13:54:06 +0900

    Update 1202.py
    change while loop

--- 1202.py
+++ 1202.py
@@ -14,15 +14,17 @@ for i in range(m):
     bags.append(int(input.readline()))
 bags.sort()
 vw.sort()
-
+print(vw)
+print(bags)
 ans = 0
 q=[]
 t=0
 for b in bags:
-    while t <=len(vw)-1:
-        if b >= vw[t][0]:
-            heapq.heappush(q,-vw[t][1])
+    while t <=len(vw)-1 and b >=vw[t][0]:
+        heapq.heappush(q,-vw[t][1])
         t+=1
+    print(q)
     if q:
         ans += -heapq.heappop(q)
+        print(ans)
 print(ans)
