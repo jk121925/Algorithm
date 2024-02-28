8d0e2e682c177d3d6b4bb31bc78e54ba44a958bc
jk121925 <52795602+jk121925@users.noreply.github.com>
2024-02-28 11:01:25 +0900

    Update 9466.py

--- 9466.py
+++ 9466.py
@@ -1,48 +1,39 @@
 import sys
 input = sys.stdin
-def getParent(x):
-    if x == parent[x]:
-        return x
-    else:
-        return getParent(parent[x])
-def travel(x):
-    global n
-    visit = [0 for _ in range(n+1)]
-    visit[x] = 1
-    i = parent[x]
-    ret = [x]
-    while True:
-        if visit[i] == 1:
-            if i == x:
-                return ret 
-            else:
-                break
-        visit[i] =1
-        ret.append(i)
-        i = parent[i]
-        
-    return []
-    
 
-def union(a,b):
-    # pa = getParent(a)
-    # pb = getParent(b)
-    parent[a] = b
+def dfs(x):
+    global n
+    visit = [False for _ in range(n+1)]
+    s = [x]
+    ret =[]
+    while True:
+        now = s.pop(-1)
+        if visit[now]:
+            break
+        visit[now] = True
+        ret.append(now)
+        if x == parent[now]:
+            return ret
+        s.append(parent[now])
+
+        
 
 TC = int(input.readline())
 for _ in range(TC):
     n = int(input.readline())
-    parent = [i for i in range(n+1)]
-    l = list(map(int, input.readline().split(" ")))
-    ans = 0
-    for index, vector in enumerate(l):
-        union(index+1,vector)
-    global_visit = [0 for _ in range(n+1)]
+    parent =[0]+ list(map(int, input.readline().split(" ")))
+    ans = n
+    global_visit = [False for _ in range(n+1)]
     for i in range(1,n+1):
-        if global_visit[i] == 0:
-            ret_list = travel(i)
-            if ret_list:
-                ans +=len(ret_list)
-                for r in ret_list:
-                    global_visit[r] = 1
-    print(n-ans)
+        if not global_visit[i]:
+            ret = dfs(i)
+            if ret:
+                print(ret)
+                ans -=len(ret)
+                for r in ret:
+                    global_visit[r] = True
+    print(ans)  
+    
+    
+
+
