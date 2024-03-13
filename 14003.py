9db761f5bd1de1f363830ae6d3187d322ecada6e
jk121925 <52795602+jk121925@users.noreply.github.com>
2024-03-13 09:47:14 +0900

    Update 14003.py

--- 14003.py
+++ 14003.py
@@ -3,33 +3,33 @@ input = sys.stdin.readline
 
 n = int(input())
 l = list(map(int, input().split(" ")))
-ans =[]
+
 def lower_bound(s,e,target):
     while s<e:
         mid = int((e+s)/2)
-        if l[mid] <target:
+        if ans[mid][0] <target:
             s = mid+1
         else:
             e = mid
     return s
 
-path = [1 for i in range(len(l))]
 length = 0
+dp =[[l[i],1] for i in range(len(l))]
+ans=[]
 for i in range(len(l)):
     if not ans:
-        ans.append(l[i])
-    elif l[i] > ans[-1]:
-        target = len(l[:i+1]) -1 -l[:i+1][::-1].index(ans[-1])
-        path[i] = path[target]+1
-        ans.append(l[i])
+        ans.append(dp[i])
+    elif l[i] > ans[-1][0]:
+        dp[i][1] = ans[-1][1]+1
+        ans.append(dp[i])
     else:
         id = lower_bound(0,len(ans)-1,l[i])
-        ans[id] = l[i]
-        path[i] = path[id]
-    length = max(path[i],length)
-ret = []
+        dp[i][1] = ans[id][1]
+        ans[id] = dp[i]
+    length = max(length,dp[i][1])
+ret = ""
 for i in range(len(l)-1,-1,-1):
-    if length == path[i]:
-        ret.append(l[i])
+    if length == dp[i][1]:
+        ret = " " + str(dp[i][0]) + ret
         length-=1
-print(*list(reversed(ret)),sep=" ")
+print(ret.strip())
