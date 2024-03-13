b353861372d8a7d1c489ef3e73469819c1521a31
jk121925 <52795602+jk121925@users.noreply.github.com>
2024-03-13 09:10:00 +0900

    Update 14003.py

--- 14003.py
+++ 14003.py
@@ -13,12 +13,23 @@ def lower_bound(s,e,target):
             e = mid
     return s
 
+path = [1 for i in range(len(l))]
+length = 0
 for i in range(len(l)):
     if not ans:
         ans.append(l[i])
     elif l[i] > ans[-1]:
+        target = len(l[:i+1]) -1 -l[:i+1][::-1].index(ans[-1])
+        path[i] = path[target]+1
         ans.append(l[i])
     else:
         id = lower_bound(0,len(ans)-1,l[i])
         ans[id] = l[i]
-print(*ans,sep=" ")
+        path[i] = path[id]
+    length = max(path[i],length)
+ret = []
+for i in range(len(l)-1,-1,-1):
+    if length == path[i]:
+        ret.append(l[i])
+        length-=1
+print(*list(reversed(ret)),sep=" ")
