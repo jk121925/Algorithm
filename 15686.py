f9d63a3b5cf6a4f1a3e5d6246cdd19ecbd60813a
jk121925 <52795602+jk121925@users.noreply.github.com>
2024-03-15 09:05:58 +0900

    Update 15686.py

--- 15686.py
+++ 15686.py
@@ -4,23 +4,20 @@ input = sys.stdin.readline
 def dist(a,b):
     return abs(a[0]-b[0]) + abs(a[1]-b[1])
 
-def solve(num):
+def solve(index,num):
     global M,ret
     if num == M:
-        local_sum =0 
-        for h in house:
-            temp =1e9
-            for ch in range(len(checken_house)):
-                if alive[ch]:
-                    temp= min(temp,dist(h,checken_house[ch]))
-            local_sum += temp
+        local_sum =0
+        for i in range(len(checken_house)):
+            if alive[i]:
+                local_sum += chichen_dist[i]
         ret = min(ret,local_sum)
         return 
     else:
-        for i in range(len(checken_house)):
+        for i in range(index,len(checken_house)):
             if alive[i]:
                 alive[i] = False
-                solve(num-1)
+                solve(index+1,num-1)
                 alive[i] = True
     return
 
@@ -39,5 +36,12 @@ for i in range(N):
             house.append((i,j))
 
 alive = [True for _ in range(len(checken_house))]
-solve(len(checken_house))
+chichen_dist = [0 for i in range(len(checken_house))]
+
+for ch in range(len(checken_house)):
+    dist_ = 0
+    for h in house:
+        dist_ += dist(h,checken_house[ch])
+    chichen_dist[ch] = dist_
+solve(0,len(checken_house))
 print(ret)
