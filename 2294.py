b41a92998e868876335057b3a613f54870c06051
jk121925 <52795602+jk121925@users.noreply.github.com>
2024-03-20 08:38:19 +0900

    Update 2294.py

--- 2294.py
+++ 2294.py
@@ -6,19 +6,14 @@ l = [int(input()) for i in range(n)]
 l.sort()
 
 
-dp = [[1e9 for i in range(k+1)] for i in range(k+1)]
+dp = [1e9 for i in range(k+1)]
+dp[0]=0
 for c in l:
-    dp[1][c] = 1
-flag = False
+    dp[c] = 1
 for i in range(2,k+1):
-    for j in range(k+1):
-        for c in l:
-            if j-c>0:
-                dp[i][j] = min(dp[i][j],dp[i-1][j-c]+1)
-                if j == k and dp[i][j]!=1e9:
-                    flag = True
-    if flag:
-        print(dp[i][k])
-        break
-if not flag:
-    print(-1)
+    m = 1e9
+    for c in l:
+        if i-c>0:
+            m = min(dp[i-c]+1,min(dp[i],m)) 
+    dp[i] = m
+print(dp[-1])
