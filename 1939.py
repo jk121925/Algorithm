e1271712efb68fe3357ce3ca857ca8d524c03cd1
jk121925 <52795602+jk121925@users.noreply.github.com>
2024-03-19 16:40:42 +0900

    Update 1939.py

--- 1939.py
+++ 1939.py
@@ -12,18 +12,35 @@ for i in range(m):
     dic[b].append((c,a))
 
 start,end = map(int, input().split(" "))
-def solve(start):
-    global n,m
-    dist = [0 for i in range(n+1)]
-    q = []
-    heapq.heappush(q,(0,start))
-    dist[start] = 0
-    while q:
-        now = heapq.heappop(q)
-        for d in dic[now[1]]:
-            if dist[d[1]] < d[0] or dist[d[1]] < dist[now[1]]:
-                dist[d[1]] = max(d[0],dist[now[0]])
-                heapq.heappush(q,d)
 
-    return dist
-print(solve(start)[end])
+def solve():
+    global n,m
+    s = 0
+    e = 1000000000
+    while s<=e:
+        mid = (s+e)//2
+        if bfs(mid):
+            e = mid-1
+        else:
+            s = mid+1
+    return s
+
+def bfs(value):
+    global n,m,start,end
+    q = Queue()
+    q.put((0,start))
+    visit = [False for i in range(n+1)]
+    visit[start] = True
+    while not q.empty():
+        now = q.get()
+        if now[1] == end:
+            return True
+        for d in dic[now[1]]:
+            if d[0] <= value:
+                q.put(d)
+                visit[d[1]] =True
+            else:
+                return False
+    return False        
+
+print(solve())
