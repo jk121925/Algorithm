2fb8546ee52159a06b709ef328285a4a3b5a3e84
jk121925 <52795602+jk121925@users.noreply.github.com>
2024-03-19 13:24:29 +0900

    Update 9935.py

--- 9935.py
+++ 9935.py
@@ -5,14 +5,10 @@ s = input().strip()
 m = input().strip()
 
 stack = []
-i =len(s)-1
 for i in s:
     stack.append(i)
     if stack[-1] == m[-1] and stack[-len(m):]:
-        cnt =0 
-        while cnt <len(m):
-            stack.pop()
-            cnt+=1
+        del stack[-len(m)]
 if len(stack) !=0:
     print("".join(stack))
 else:
