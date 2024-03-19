7de49f96dd8e6fcbe5f47a8cfe7d4a35fdfeb26d
jk121925 <52795602+jk121925@users.noreply.github.com>
2024-03-19 15:43:20 +0900

    Create 1939.py

---
+++ 1939.py
@@ -0,0 +1,30 @@
+import sys
+from queue import Queue
+import heapq
+input = sys.stdin.readline
+INF = 1e10
+n,m = map(int,input().split(" "))
+dic = {i:[] for i in range(1,n+1)}
+
+for i in range(m):
+    a,b,c = map(int,input().split(" "))
+    dic[a].append((c,b))
+    dic[b].append((c,a))
+
+start,end = map(int, input().split(" "))
+def solve(start):
+    global n,m
+    dist = [0 for i in range(n+1)]
+    q = []
+    heapq.heappush(q,(0,start))
+    dist[start] = 0
+    while q:
+        now = heapq.heappop(q)
+        for d in dic[now[1]]:
+            if dist[d[1]] < d[0]:
+                dist[d[1]] = d[0]
+                heapq.heappush(q,d)
+
+    
+    return max(dist)
+print(solve(start))
