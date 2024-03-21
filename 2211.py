55bd94961e599c98295d70696abcb0e64914d62e
jk121925 <52795602+jk121925@users.noreply.github.com>
2024-03-22 08:32:20 +0900

    Update 2211.py

--- 2211.py
+++ 2211.py
@@ -1,3 +1,4 @@
+mport sys
 import heapq
 input = sys.stdin.readline
 
@@ -7,25 +8,28 @@ dic = {i:[] for i in range(N+1)}
 
 for _ in range(M):
     a,b,c = map(int, input().split(" "))
-    dic[a].append((c,b,a))
-    dic[b].append((c,a,b))
+    dic[a].append((c,b))
+    dic[b].append((c,a))
 
 ans =[]
 
-q = []
-visit = [False for i in range(N+1)]
-heapq.heappush(q,(0,1))
+dist = [[1e9,-1] for i in range(N+1)]
 
+q = []
+q.append((0,1))
+dist[1][1] = 0
+dist[1][0] = 1
 while q:
     now = heapq.heappop(q)
-    if visit[now[1]] ==True:
-        continue
-    visit[now[1]] = True
-    ans.append(now)
     for d in dic[now[1]]:
-        if visit[d[1]] ==False:
-            heapq.heappush(q,d)
-print(len(ans)-1)
-ans_process = ans[1:]
-ans_process = list(map(lambda x : str(x[1]) +" "+ str(x[2]), ans_process))
-print(*ans_process,sep="\n")
+        if dist[d[1]][0] > dist[now[1]][0] + d[0]:
+            dist[d[1]][0] = dist[now[1]][0] +d[0]
+            dist[d[1]][1] = now[1]
+            q.append(d)
+ans = []
+for i in range(2,N+1):
+    ans.append(str(i) +" "+ str(dist[i][1]))
+print(len(ans))
+print(*ans,sep="\n")
+
+
